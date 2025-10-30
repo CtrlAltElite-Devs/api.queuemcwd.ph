2adc37d2458a2c689823e226fe621407de2b5dfa
y4nder <lorenzolubguban@gmail.com>
2025-10-31 05:41:56 +0800

    feat: add NextMonthDaySeeder to handle seeding for the next month

--- src/seeders/database.seeder.ts
+++ src/seeders/database.seeder.ts
@@ -2,12 +2,13 @@ import { EntityManager } from "@mikro-orm/core";
 import { Seeder } from "@mikro-orm/seeder";
 import { Logger } from "@nestjs/common";
 import { MonthDaySeeder } from "./month-day.seeder";
+import { NextMonthDaySeeder } from "./next-month-day.seeder";
 
 export class DatabaseSeeder extends Seeder{
     private readonly logger = new Logger(DatabaseSeeder.name)
     async run(em: EntityManager): Promise<void> {
         this.logger.log("Seeding Database...");
-        return this.call(em, [MonthDaySeeder])
+        return this.call(em, [MonthDaySeeder, NextMonthDaySeeder])
     }
 
 }
