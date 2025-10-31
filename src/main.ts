9034aed0072600390d06a513c4de87273747c769
y4nder <lorenzolubguban@gmail.com>
2025-11-01 00:37:51 +0800

    feat: add @scalar/nestjs-api-reference and integrate Scalar UI into API documentation

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe, VersioningType } from "@nestjs/common";
 import { InitializeDatabase } from "./configurations/database-initializer.config";
-import { useSwagger } from "./configurations/open-api.config";
+import { UseApiDocumentations } from "./configurations/open-api.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,13 +12,13 @@ async function bootstrap() {
   await InitializeDatabase(app);
 
   app.setGlobalPrefix("api");
-  useSwagger(app);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: "1",
   });
 
+  UseApiDocumentations(app);
   app.enableCors({ origin: true, credentials: true });
 
   await app.listen(process.env.PORT ?? 5000);
