9117b2b267bb38e032e2992609a68e5fd1822895
leander lubguban <lorenzolubguban@gmail.com>
2025-11-02 14:55:28 +0800

    hotfix: branch module dependency

--- src/modules/branch/branch.module.ts
+++ src/modules/branch/branch.module.ts
@@ -2,12 +2,13 @@ import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Module } from "@nestjs/common";
 import { Admin } from "src/entities/admin.entity";
 import { Branch } from "src/entities/branch.entity";
+import { AdminModule } from "../admin/admin.module";
 import { CommonModule } from "../common/common.module";
 import { BranchController } from "./branch.controller";
 import { BranchService } from "./branch.service";
 
 @Module({
-    imports: [MikroOrmModule.forFeature([Branch, Admin]), CommonModule],
+    imports: [MikroOrmModule.forFeature([Branch, Admin]), CommonModule, AdminModule],
     controllers: [BranchController],
     providers: [BranchService],
 })
