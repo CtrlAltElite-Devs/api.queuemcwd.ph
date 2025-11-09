00effe15da4c82a6dab732c0cbfe5622bb5ee21d
Leander Lubguban <113151776+y4nder@users.noreply.github.com>
2025-11-09 13:26:12 +0800

    [MCWD-9] Add functionality for Admin to get all appointments for a specific day #29
    feat (admin): functionality to get all appointments for a specific day by admin (#29)

--- src/constants/error-messages/appointment.error.ts
+++ src/constants/error-messages/appointment.error.ts
@@ -1,10 +1,13 @@
-export const SLOT_ID_NOT_FOUND = "Slot ID not found";
-export const SLOT_NOT_ON_WORKING_DAY = "Slot is not on a working day";
-export const SLOT_ALREADY_ENDED = "Slot has already ended";
-export const SLOT_NOT_ACTIVE = "Slot is not active";
-export const SLOT_TODAY_NOT_ALLOWED = "You cannot create an appointment for today";
-export const SLOT_DATE_PASSED = "Slot date has already passed";
+export enum SlotErrors {
+    SLOT_ID_NOT_FOUND = "Slot ID not found",
+    SLOT_NOT_ON_WORKING_DAY = "Slot is not on a working day",
+    SLOT_ALREADY_ENDED = "Slot has already ended",
+    SLOT_NOT_ACTIVE = "Slot is not active",
+    SLOT_TODAY_NOT_ALLOWED = "You cannot create an appointment for today",
+    SLOT_DATE_PASSED = "Slot date has already passed",
+    SLOT_FULL = "Slot is already full",
+}
+
 export const SLOT_TOO_FAR = (allowedTimeFrame: number) => {
     return `Cannot create an appointment on slots ${allowedTimeFrame} days in the future`;
 };
-export const SLOT_FULL = "Slot is already full";
---
+++ src/constants/error-messages/branch-admin.error.ts
@@ -0,0 +1,3 @@
+export enum BranchAdminError {
+    NOT_ASSIGNED = "Admin is not assigned to this branch",
+}
