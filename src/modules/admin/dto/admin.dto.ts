68366d702f8963d437594609b498dcbcac8f69c7
y4nder <lorenzolubguban@gmail.com>
2025-11-15 04:03:45 +0800

    hotfix(admin): admin routes and me endpoint

--- src/modules/admin/dto/admin.dto.ts
+++ src/modules/admin/dto/admin.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Admin, AdminRole } from "src/entities/admin.entity";
+import { BranchDto } from "src/modules/branch/dto/branch.dto";
 
 export class AdminDto {
     @ApiProperty()
@@ -14,12 +15,16 @@ export class AdminDto {
     @ApiProperty()
     branchId?: string;
 
+    @ApiProperty()
+    branch?: BranchDto;
+
     static Map(admin: Admin): AdminDto {
         const dto = new AdminDto();
         dto.id = admin.id;
         dto.email = admin.email;
         dto.role = admin.role;
         dto.branchId = admin.branch?.id;
+        dto.branch = admin.branch ? BranchDto.Map(admin.branch) : undefined;
         return dto;
     }
 }
