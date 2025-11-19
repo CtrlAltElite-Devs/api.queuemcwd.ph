edc32c1f0f5fc48b83f834960f2e4cce99a74d08
Leander Lubguban <113151776+y4nder@users.noreply.github.com>
2025-11-19 22:01:05 +0800

    (Staging) [MCWD-15] (#48) (#49)
    * [MCWD-15] (#48)
    
    * feat: enhance linting process and add Discord notifications; update test configurations
    
    * feat: minor adjustment
    
    * feat: add Discord notifications for deployment status and set NODE_ENV to production
    
    * fix: [MCWD-15] #50
    
    fix: update Discord notification messages for production deployment (#50)

--- src/configurations/env/server.env.ts
+++ src/configurations/env/server.env.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const serverEnvSchema = z.object({
     PORT: z.coerce.number().default(5000),
-    NODE_ENV: z.enum(["development", "production"]).default("development"),
+    NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
 });
 
 export type ServerEnv = z.infer<typeof serverEnvSchema>;
